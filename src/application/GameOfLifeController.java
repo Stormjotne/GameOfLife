851d144700b83a0f758cda56dbd2d952791b08ea
Håkon André Heskja <h.heskja@gmail.com>
2017-02-09 18:42:10 +0100

    Gjort litt forandringer og prøvd Canvas. Trenger gjelp av veileder

--- src/application/GameOfLifeController.java
+++ src/application/GameOfLifeController.java
@@ -3,12 +3,49 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class GameOfLifeController implements javafx.fxml.Initializable {
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class GameOfLifeController extends Application implements javafx.fxml.Initializable {
+	
+	@FXML private Canvas grid;
+	private GraphicsContext gc;
+	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+		draw(gc);
 	}
 	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		primaryStage.setTitle("Game Of Life");
+		Parent root = FXMLLoader.load(getClass().getResource("GameOfLifeFXML.fxml"));
+		Scene scene = new Scene(root);
+		
+        
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	@FXML private void draw(GraphicsContext gc) {
+		gc.setFill(Color.BLACK);
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+		GameOfLifeModel game = new GameOfLifeModel();
+		
+	}
 }
--- src/application/GameOfLifeModel.java
+++ src/application/GameOfLifeModel.java
@@ -1,5 +1,14 @@
 package application;
 
 public class GameOfLifeModel {
-
+	private byte[][] board = {
+			{1, 0, 0, 1},
+			{0, 0, 1, 1},
+			{1, 0, 1, 0},
+			{0, 1, 0, 0}};
+	
+	public GameOfLifeModel(){
+		System.out.println("Game made!");
+	}
+	
 }
