8815add1d599afda9ff3a0d0e6a63a3e1dea7881
Håkon André Heskja <Heskja@Hakons-MacBook-Pro-2.local>
2017-02-09 16:44:38 +0100

    Et utgangspunkt og forbedret GUI. Også for å teste at Git fungerer

---
+++ src/application/GameOfLifeController.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameOfLifeController implements javafx.fxml.Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
