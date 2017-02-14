a7a43cb0def056e7757ca0369fa666e2d9944218
JuliaSofieTeppen <julia.teppen@gmail.com>
2017-02-14 08:32:18 +0100

    Random matrix

--- src/application/GameOfLifeModel.java
+++ src/application/GameOfLifeModel.java
@@ -1,13 +1,28 @@
 package application;
 
 public class GameOfLifeModel {
+	private int k = 35, m = 60;
+	public byte[][] board = new byte[k][m];
+	/*
 	private byte[][] board = new byte[][] {
 			{1, 0, 0, 1},
 			{0, 0, 1, 1},
 			{1, 0, 1, 0},
 			{0, 1, 0, 0}};
+	*/
 	
 	public GameOfLifeModel(){
+Random ranNum = new Random();
+		
+		for(int i = 0; i < board.length; i++){
+			
+			for(int j = 0; j < board[i].length; j++){
+				byte n = (byte)ranNum.nextInt(2);
+				board[i][j] = n;
+				
+			}
+		}
+		
 		System.out.println("Game made!");
 	}
 	
