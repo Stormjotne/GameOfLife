edd30630f54d6b61cc3d244578ce142891935910
JuliaSofieTeppen <julia.teppen@gmail.com>
2017-04-20 13:04:21 +0200

    added timer.

--- src/application/GameOfLifeController.java
+++ src/application/GameOfLifeController.java
@@ -1,4 +1,4 @@
-package application; /*       MY VERSION        */
+package application;
 
 import java.net.URL;
 import java.util.Optional;
@@ -52,6 +52,7 @@ public class GameOfLifeController extends Application implements javafx.fxml.Ini
 	@FXML private Button fileChooserButton;
 	@FXML private Button fileByURLButton;
 	@FXML public ColorPicker colorPicker;
+	@FXML private Slider speed;
 	
 	private GraphicsContext gc;
 	public GameOfLifeModel game;
@@ -61,6 +62,17 @@ public class GameOfLifeController extends Application implements javafx.fxml.Ini
 	FileChooser.ExtensionFilter extentionFilter = new FileChooser.ExtensionFilter("RLE files (*.rle)", "*.rle");
 	File defaultDirectory = new File("patterns/");
 	Timeline animation = new Timeline(new KeyFrame(Duration.millis(40), e -> run()));
+	
+	int timing = 120;
+	Timeline animation = new Timeline(new KeyFrame(Duration.millis(1000), e -> run())); 
+	    
+	public void timerlistener(){
+        speed.valueProperty().addListener((ObservableValue<? extends Number> timerlistener, Number oldtime, Number newtime) -> {
+            timing = newtime.intValue();
+            animation.setRate(timing);
+        });
+    }
+
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -92,7 +104,95 @@ public class GameOfLifeController extends Application implements javafx.fxml.Ini
 		
 		grid.addEventHandler(MouseEvent.MOUSE_DRAGGED,(MouseEvent e) ->{
 			int x = (int)(e.getX()/game.getCellSize());
-			int y = (int)(e.getY()/game.getCellSize());
+			int y = (int)(e.getY()/game.getCellSize());<?xml version="1.0" encoding="UTF-8"?>
+
+<?import java.lang.*?>
+<?import javafx.geometry.*?>
+<?import javafx.scene.canvas.*?>
+<?import javafx.scene.control.*?>
+<?import javafx.scene.layout.*?>
+
+<BorderPane maxHeight="-Infinity" maxWidth="-Infinity" minHeight="-Infinity" minWidth="-Infinity" prefHeight="600.0" prefWidth="1080.0" xmlns="http://javafx.com/javafx/8" xmlns:fx="http://javafx.com/fxml/1" fx:controller="application.GameOfLifeController">
+   <center>
+      <Canvas fx:id="grid" height="400.0" width="800.0" BorderPane.alignment="CENTER" />
+   </center>
+   <bottom>
+      <GridPane prefHeight="52.0" prefWidth="1030.0" BorderPane.alignment="CENTER">
+        <columnConstraints>
+            <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="210.66668701171875" minWidth="0.0" prefWidth="63.666656494140625" />
+            <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="432.0" minWidth="0.0" prefWidth="68.33334350585938" />
+            <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="619.0" minWidth="10.0" prefWidth="70.66668701171875" />
+            <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="619.0" minWidth="10.0" prefWidth="85.33331298828125" />
+            <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="474.66668701171875" minWidth="10.0" prefWidth="68.0" />
+            <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="474.66668701171875" minWidth="10.0" prefWidth="156.0" />
+            <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="474.66668701171875" minWidth="10.0" prefWidth="118.0" />
+          <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="818.0000305175781" minWidth="10.0" prefWidth="123.0" />
+            <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="818.0000305175781" minWidth="10.0" prefWidth="154.0" />
+          <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" maxWidth="383.0" minWidth="10.0" prefWidth="204.0" />
+        </columnConstraints>
+        <rowConstraints>
+          <RowConstraints minHeight="10.0" prefHeight="30.0" vgrow="SOMETIMES" />
+        </rowConstraints>
+         <children>
+            <Button fx:id="playButton" defaultButton="true" mnemonicParsing="false" text="Play">
+               <padding>
+                  <Insets bottom="5.0" left="10.0" right="10.0" top="5.0" />
+               </padding></Button>
+            <Button fx:id="pauseButton" mnemonicParsing="false" text="Pause" GridPane.columnIndex="1">
+               <padding>
+                  <Insets bottom="5.0" left="10.0" right="10.0" top="5.0" />
+               </padding>
+            </Button>
+            <Button fx:id="stopButton" mnemonicParsing="false" text="Reset" GridPane.columnIndex="2">
+               <padding>
+                  <Insets bottom="5.0" left="10.0" right="10.0" top="5.0" />
+               </padding>
+            </Button>
+            <Button fx:id="randomButton" mnemonicParsing="false" text="Random" GridPane.columnIndex="3" />
+            <Button fx:id="cleanButton" mnemonicParsing="false" text="Clean" GridPane.columnIndex="4" />
+            <Button fx:id="fileChooserButton" mnemonicParsing="false" text="Pattern From Drive" GridPane.columnIndex="5" />
+            <GridPane GridPane.columnIndex="9">
+              <columnConstraints>
+                <ColumnConstraints hgrow="SOMETIMES" minWidth="10.0" prefWidth="100.0" />
+                <ColumnConstraints hgrow="SOMETIMES" minWidth="10.0" prefWidth="100.0" />
+              </columnConstraints>
+              <rowConstraints>
+                <RowConstraints minHeight="10.0" prefHeight="30.0" vgrow="SOMETIMES" />
+              </rowConstraints>
+               <children>
+                  <Label text="Cell size:" GridPane.halignment="RIGHT" />
+                  <TextField text="10" GridPane.columnIndex="1">
+                     <GridPane.margin>
+                        <Insets left="10.0" right="10.0" />
+                     </GridPane.margin>
+                  </TextField>
+               </children>
+            </GridPane>
+            <Button fx:id="fileByURLButton" mnemonicParsing="false" text="Pattern From URL" GridPane.columnIndex="6" />
+            <ColorPicker fx:id="colorPicker" prefHeight="25.0" prefWidth="87.0" GridPane.columnIndex="7">
+               <GridPane.margin>
+                  <Insets left="10.0" right="10.0" />
+               </GridPane.margin>
+            </ColorPicker>
+            <GridPane GridPane.columnIndex="8">
+              <columnConstraints>
+                <ColumnConstraints hgrow="SOMETIMES" maxWidth="109.0" minWidth="10.0" prefWidth="31.0" />
+                <ColumnConstraints hgrow="SOMETIMES" maxWidth="194.0" minWidth="10.0" prefWidth="120.0" />
+              </columnConstraints>
+              <rowConstraints>
+                <RowConstraints minHeight="10.0" prefHeight="30.0" vgrow="SOMETIMES" />
+              </rowConstraints>
+               <children>
+                  <Slider blockIncrement="0.0" majorTickUnit="2.0" max="20.0" min="1.0" minorTickCount="0" showTickLabels="true" showTickMarks="true" snapToTicks="true" value="1.0" GridPane.columnIndex="1" />
+                  <Label text="FPS:" />
+               </children>
+            </GridPane>
+         </children>
+      </GridPane>
+   </bottom>
+   
+</BorderPane>
+
 			
 				game.changeSingleBoardValueToOne(x,y);
 				drawBox(x,y,colorPicker.getValue());
@@ -138,6 +238,7 @@ public class GameOfLifeController extends Application implements javafx.fxml.Ini
 	
 	public void run(){
 		draw(gc);
+		timerlistener();
 		rules.nextGeneration();
 	}
 	
